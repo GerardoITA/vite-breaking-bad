b54e9ff0496bdf644c6f1878492c05ac549e7b6a
GerardoITA <gerardo.giusti@tiscali.it>
2022-12-15 08:41:08 +0100

    finished

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,8 +1,14 @@
 <script>
 export default {
     name: "Cards",
+    props :{
+        immagine: String,
+        nome: String,
+        status: String,
+    },
     data() {
         return {
+            
         }
     }
 }
@@ -10,7 +16,9 @@ export default {
 
 <template>
     <div class="card">
-        
+        <img :src="immagine" :alt="nome">
+        <h3>{{nome}}</h3>
+        <h5>{{status}}</h5>
     </div>
 </template>
 
@@ -20,5 +28,15 @@ export default {
         width: 200px;
         border-radius: 25px;
         background-color: black;
+        padding: 10px;
+        
+    }
+    img {
+        width: 200px;
+        height: auto;
+        border-radius: 100px;
+    }
+    h3, h5 {
+        color: white;
     }
 </style>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -18,11 +18,18 @@ console.log(store.characterList.length)
 </script>
 
 <template>
+    <h1>Assolutamente breaking bad api giuro</h1>
     <div v-for="card in store.characterList">
-        <Cards></Cards>
+        <Cards
+        :immagine="card.image"
+        :nome="card.name"
+        :status="card.status"></Cards>
     </div>
    
 </template>
 
 <style scoped>
+    h1 {
+        color: black;
+    }
 </style>
